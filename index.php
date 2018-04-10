d3c02654fcc21f12eb0c8195fcf17fb2993e75b4
sahilyadav8569 <38242904+sahilyadav8569@users.noreply.github.com>
2018-04-10 13:58:47 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,12 @@
-hello. How are you ?
-This is my first php page
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My first PHP page</h1>
+
+<?php
+echo "Hello World! this is my first php page";
+?>
+
+</body>
+</html>
